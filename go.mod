0bfa811cd2c7edb8e14276746b5181bd5e3bae50
hndmin <andysjhan2@naver.com>
2023-08-02 19:38:59 +0900

    d1

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module router/main
 
 go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func checkURL(w http.ResponseWriter, r *http.Request) {
+	//w.Write([]byte("Hello World!!!!"))
+	keyParam := chi.URLParam(r, "key")
+
+	//w.Write([]byte(keyParam))
+	if keyParam == "hello" {
+		w.Write([]byte("World!"))
+	} else if keyParam == "world" {
+		w.Write([]byte("Hello!"))
+	}
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Get the requested URL from the request
+	requestedURL := r.URL.String()
+
+	// Print the requested URL
+	fmt.Println("Requested URL:", requestedURL)
+
+	// Send a response back to the client
+	fmt.Fprintf(w, "Requested URL: %s", requestedURL)
+
+	paramArr := [3]string{"/", "hello", "world"}
+	for i := 0; i < 3; i++ {
+
+	}
+
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	// 1 ////////////////////////////////////////////////////////////
+	r.Use(middleware.Logger)
+	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello World!!!!"))
+	// })
+	// r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("World!"))
+	// })
+	// r.Get("/world", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello!"))
+	// })
+	// 2 ////////////////////////////////////////////////////////////
+
+	http.HandleFunc("/", handler)
+
+	//r.Get("/queryparam", checkURL)
+	//http.HandleFunc("/", handler)
+
+	//
+	http.ListenAndServe(":3000", nil)
+
+	// fmt.Println("Hello, World!!")
+
 }
